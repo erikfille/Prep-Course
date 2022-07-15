46b04152166dbbc6b22198363225b94d1628649e
ErikFille <erikfilleaudeau@gmail.com>
2022-07-15 14:36:48 -0300

    JSI - Homework - Complete

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -71,9 +71,14 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código: 
-  return str1.lenght == str2.lenght;
-  
+  var longitud1 = str1.length;
+  var longitud2 = str2.length;
+  if (longitud1 == longitud2) {
+      return true;
+  }
+    return false;
 }
+
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
@@ -153,8 +158,13 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
-
+  if (numero > 1) {
+    return 'Es positivo';
+  }
+  if (numero < 0) {
+    return 'Es negativo';
+  }
+  return false
 }
 
 function agregarSimboloExclamacion(str) {
@@ -217,8 +227,26 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra == 'a'; 'e'; 'i'; letra == 'o'; letra == 'u') { return 'Es Vocal';} return 'Dato Incorrecto' 
-  
+  var longitud = letra.length
+  if (longitud > 1){
+    return 'Dato incorrecto'
+  }
+  if (letra == 'a') {
+    return 'Es vocal';
+  }
+  if (letra == 'e') {
+    return 'Es vocal';
+  }
+  if (letra == 'i') {
+    return 'Es vocal';
+  }
+  if (letra == 'o') {
+    return 'Es vocal';
+  }
+  if (letra == 'u') {
+    return 'Es vocal';
+  }
+  return 'Dato incorrecto';
 }
 
 
